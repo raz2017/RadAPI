89e3216b27d4700ff09e1834b29b5332dd6c1ce5
Raziel Melchor <razielmelchor@gmail.com>
2023-08-14 03:26:26 -0700

    updated tests

--- tests/UnitTests/AccountServiceTests.cs
+++ tests/UnitTests/AccountServiceTests.cs
@@ -1,4 +1,5 @@
 using Moq;
+using RadancyAPI.Domain;
 using RadancyAPI.Services;
 
 namespace UnitTests;
@@ -22,7 +23,153 @@ public class AccountServiceTests
         Assert.NotEqual(Guid.Empty, result.Id);
     }
 
-    private IAccountService CreateTarget(Mock<IUserService> userService)
+    [Fact]
+    public void GetAccount_ValidUserIdAndAccountId_ReturnsAssociatedAccount()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+        var target = CreateTarget(userServiceMock);
+
+        //Getting account presupposes account exists
+        var userId = Guid.NewGuid();
+        var account = new Account();
+        target.Accounts.Add(userId, new() { { account.Id, account } });
+
+        //Act
+        var result = target.GetAccount(userId, account.Id);
+
+        //Assert
+        Assert.Equal(100, result.Balance);
+        Assert.Equal(account.Id, result.Id);
+    }
+
+    [Fact]
+    public void DeleteAccount_ValidUserIdAndAccountId_AccountIsDeleted()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+
+        var target = CreateTarget(userServiceMock);
+
+        //Deleting account presupposes account exists
+        var userId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        target.Accounts.Add(userId, new() { { accountId, new Account() } });
+
+        //Act
+        target.RemoveAccount(userId, accountId);
+
+        //Assert
+        Assert.Empty(target.Accounts[userId]);
+    }
+
+    [Fact]
+    public void UpdateAccount_UserDeposit_ReturnsUpdatedAccount()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+
+        var target = CreateTarget(userServiceMock);
+
+        //Update to accounts presupposes account already exists
+        var userId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        target.Accounts.Add(userId, new(){{accountId, new Account(150)}});
+
+        //Act
+        var result = target.UpdateAccount(userId, accountId, AccountAction.Deposit, 100);
+
+        //Assert
+        Assert.Equal(250, result.Balance);
+        Assert.NotEqual(accountId, result.Id);
+    }
+
+    [Fact]
+    public void UpdateAccount_UserWithdrawal_ReturnsUpdatedAccount()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+
+        var target = CreateTarget(userServiceMock);
+
+        //Update to accounts presupposes account already exists
+        var userId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        target.Accounts.Add(userId, new() { { accountId, new Account(1000) } });
+
+        //Act
+        var result = target.UpdateAccount(userId, accountId, AccountAction.Withdraw, 100);
+
+        //Assert
+        Assert.Equal(900, result.Balance);
+        Assert.NotEqual(accountId, result.Id);
+    }
+
+    [Fact]
+    public void UpdateAccount_UserBalanceWouldBeLessThan100_ThrowsValidationException()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+
+        var target = CreateTarget(userServiceMock);
+
+        //Update to accounts presupposes account already exists
+        var userId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        target.Accounts.Add(userId, new() { { accountId, new Account(200) } });
+
+        //Act + Assert
+        var exception = Assert.Throws<ArgumentException>(() => target.UpdateAccount(userId, accountId, AccountAction.Withdraw, 101));
+
+        Assert.Equal("An account cannot have less than $100 at any time.", exception.Message);
+    }
+
+    [Fact]
+    public void UpdateAccount_UserWithdrawsMoreThan90PercentOfBalance_ThrowsValidationException()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+
+        var target = CreateTarget(userServiceMock);
+
+        //Update to accounts presupposes account already exists
+        var userId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        target.Accounts.Add(userId, new() { { accountId, new Account(2000) } });
+
+        //Act + Assert
+        var exception = Assert.Throws<ArgumentException>(() => target.UpdateAccount(userId, accountId, AccountAction.Withdraw, 1850));
+
+        Assert.Equal("A user cannot withdraw more than 90% of their total balance from an account in a single transaction.", exception.Message);
+    }
+
+    [Fact]
+    public void UpdateAccount_UserDepositsMoreThan10000_ThrowsValidationException()
+    {
+        //Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.UserExists(It.IsAny<Guid>())).Returns(true);
+
+        var target = CreateTarget(userServiceMock);
+
+        //Update to accounts presupposes account already exists
+        var userId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        target.Accounts.Add(userId, new() { { accountId, new Account() } });
+
+        //Act + Assert
+        var exception = Assert.Throws<ArgumentException>(() => target.UpdateAccount(userId, accountId, AccountAction.Deposit, 10001));
+
+        Assert.Equal("A user cannot deposit more than $10,000 in a single transaction.", exception.Message);
+    }
+
+    private AccountService CreateTarget(Mock<IUserService> userService)
     {
         return new AccountService(userService.Object);
     }
--- tests/UnitTests/UserServiceTests.cs
+++ tests/UnitTests/UserServiceTests.cs
@@ -6,11 +6,10 @@ namespace UnitTests;
 public class AccountServiceTest
 {
     [Fact]
-    public void AddUser_AnyUser_ReturnsCreatedUserWithId()
+    public void AddUser_NewUser_ReturnsCreatedUserWithValidId()
     {
         //Arrange
         var target = GetTarget();
-
         User user = CreateUser();
 
         //Act
@@ -25,16 +24,48 @@ public class AccountServiceTest
         Assert.Equal(user.DOB, result.DOB);
     }
 
-    private IUserService GetTarget()
+    [Fact]
+    public void GetUser_ExistingUser_ReturnsExistingUser()
+    {
+        //Arrange
+        var target = GetTarget();
+        User user = CreateUser();
+        target.Users.Add(user.Id, user);
+
+        //Act
+        var result = target.GetUser(user.Id);
+
+        //Assert
+        Assert.NotEqual(Guid.Empty, result.Id);
+
+        Assert.Equal(user.FirstName, result.FirstName);
+        Assert.Equal(user.LastName, result.LastName);
+        Assert.Equal(user.Email, result.Email);
+        Assert.Equal(user.DOB, result.DOB);
+    }
+
+    [Fact]
+    public void RemoveUser_ExistingUser_UserIsDeleted()
+    {
+        //Arrange
+        var target = GetTarget();
+        User user = CreateUser();
+        target.Users.Add(user.Id, user);
+
+        //Act
+        target.RemoveUser(user.Id);
+
+        //Assert
+        Assert.False(target.Users.ContainsKey(user.Id));
+    }
+
+    private UserService GetTarget()
     {
         return new UserService();
     }
 
     private User CreateUser()
     {
-        return new User("Raziel",
-            "Melchor",
-            "razielmelchor@gmail.com",
-            DateTime.Now);
+        return new User("Raziel", "Melchor", "razielmelchor@gmail.com", DateTime.Now);
     }
 }
